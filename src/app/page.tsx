be6abbe38ca63bef4e0f8557d8b88e44038dd22f
bpen-dev <214372532+bpen-dev@users.noreply.github.com>
2025-07-31 14:51:58 +0900

    タグの選択機能の実装（タグの追加は未実装）

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,18 @@
-import { getBookmarks, type Bookmark } from '@/libs/microcms';
+import { getBookmarks, getTags, type Bookmark } from '@/libs/microcms';
 import BookmarkCard from './components/BookmarkCard';
 import styles from './page.module.css';
 import BookmarkForm from './components/BookmarkForm'; 
 
 export default async function Home() {
   const bookmarks = await getBookmarks();
+  const allTags = await getTags();
 
   return (
     <main className={styles.main}>
       <h1 className={styles.title}>My Bookmarks</h1>
       
-      <div className={styles.formContainer}> {/* フォームを囲うdivを追加 */}
-        <BookmarkForm />
+      <div className={styles.formContainer}>
+        <BookmarkForm allTags={allTags} />
       </div>
 
       <div className={styles.grid}>
